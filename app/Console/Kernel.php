2fa2c00e038aa228c44a8a62b4164dad5b97c5b0
riya <riya@technowin.co.in>
2026-08-12 17:39:17 +0530

    dash

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,8 @@ class Kernel extends ConsoleKernel
         // $schedule->command('contracts:send-expiry-reminders')->dailyAt('09:00');
         // $schedule->command('contracts:send-expiry-reminders')->dailyAt('13:30')->timezone('Asia/Kolkata');
 
-        $schedule->command('contracts:send-expiry-reminders')->dailyAt('13:52')->timezone('Asia/Kolkata');
+        // $schedule->command('contracts:send-expiry-reminders')->dailyAt('13:52')->timezone('Asia/Kolkata');
+        $schedule->command('contracts:send-expiry-reminders')->dailyAt('09:00')->timezone('Asia/Kolkata');
         // $schedule->command('inspire')
         //          ->hourly();
     }
